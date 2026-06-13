6d45af2d391d71f0e2e3201e91726172e82c89e1
posmanzzz <zhongsenyuuki@gmail.com>
2026-06-13 14:55:14 +0900

    update header links

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'ぽすまん！ | mcposman.com';
+export const SITE_DESCRIPTION = '高専生エンジニアのポートフォリオ・技術ブログ。ローカルLLM、自作PC、VRChat、せどりシステム開発など。';
